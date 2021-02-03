e33d3436e9c8c13dc90bf820f42e8cdeee40a8d4
Aziz Booker <bookeraziz@gmail.com>
2021-02-03 10:28:12 -0500

    images show up on screen when request is sent

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,27 @@
 import React from "react";
-import axios from 'axios'
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
+import ImageList from './ImageList';
 
 class App extends React.Component {
+  state = { images: [] };
 
-  onSearchSubmit(term){
-    if(term.length>0){
-      axios.get('https://api.unsplash.com/search/photos',{
-        params: {query: term},
-        headers: {
-          Authorization: 'Client-ID o2mONPajVoMOYXVFST-SpJdgOM1csurSpKjOY5ywyI4'
-        }
-      })
-    }else{
-      console.log("Empty String")
+  async onSearchSubmit(term) {
+    if (term.length >= 2) {
+      const response = await unsplash.get("/search/photos", {
+        params: { query: term },
+      });
+
+      this.setState({ images: response.data.results });
+    } else {
+      console.log("Empty String");
     }
-
-
   }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar onSubmit={(term) => this.onSearchSubmit(term)} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
